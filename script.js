b835adecdb2973936904b53341443f02e2b04579
jrobbins2006 <jrobbins2006@gmail.com>
2026-02-16 12:34:59 -0500

    fixed share icon

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ const leftSide = document.querySelector('.left__side')
 shareButton.addEventListener('click', function () {
   shareTooltip.classList.toggle('active')
   leftSide.classList.toggle('hidden')
-  shareButton.classList.toggle('active')
+
+  // Toggle between the two SVG sources
+  if (shareButton.src.includes('icon-share.svg')) {
+    shareButton.src = './images/icon-share-white.svg' // or whatever you named it
+  } else {
+    shareButton.src = './images/icon-share.svg'
+  }
 })
