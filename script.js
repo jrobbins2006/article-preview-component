3cc8af11e37aae028d43d0ef9f70b381f0f88fdb
jrobbins2006 <jrobbins2006@gmail.com>
2026-02-16 10:23:56 -0500

    trying to get share tool tip to work

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const shareButton = document.querySelector('.share__button')
 const shareTooltip = document.querySelector('.share-tooltip')
+const leftSide = document.querySelector('.left__side')
 
 shareButton.addEventListener('click', function () {
   shareTooltip.classList.toggle('active')
+  leftSide.classList.toggle('hidden')
+  shareButton.classList.toggle('active')
 })
